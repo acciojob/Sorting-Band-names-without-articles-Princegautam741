a63270c42cc0821890ff06d8960d9f29e820b939
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-11 15:08:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,24 +10,20 @@ let bandNames = [
 
 // Function to sort band names without articles
 function sortBandNames(names) {
-  // Define a regular expression to match articles at the beginning of a string
   const articlesRegex = /^(a |an |the )/i;
 
-  // Sort the names lexicographically without articles
-  names.sort((a, b) => {
+  return names.sort((a, b) => {
     const trimmedA = a.replace(articlesRegex, '').toLowerCase();
     const trimmedB = b.replace(articlesRegex, '').toLowerCase();
     return trimmedA.localeCompare(trimmedB);
   });
-
-  return names;
 }
 
 // Call the sortBandNames function to sort the band names
 const sortedBandNames = sortBandNames(bandNames);
 
 // Get the ul element with id 'bands'
-const bandsList = document.getElementById('bands');
+const bandsList = document.querySelector('#bands ul');
 
 // Iterate through the sorted band names and add them to the ul as list items
 sortedBandNames.forEach(band => {
