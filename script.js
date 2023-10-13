74aa0bca620bbcff69428f4bbb0200299dbcec10
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-13 22:54:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Array of band names
-  let bandNames = [
-    'The Rolling Stones',
-    'Aerosmith',
-    'The Beatles',
-    'Guns N\' Roses',
-    'Red Hot Chili Peppers',
-    'AC/DC'
-  ];
+describe('TestingSorting', () => {
+  it('Testing article', () => {
+    cy.visit('http://localhost:3000');  // Adjust the URL if needed
 
-  // Function to sort band names without articles
-  function sortBandNames(names) {
-    const articlesRegex = /\b(?:a|an|the)\b\s*/gi;
+    // Wait for the list to have at least 1 item
+    cy.get('#bands li').should('have.length.at.least', 1);
 
-    // Sort the names without articles
-    const sortedNames = names.sort((a, b) => {
-      const trimmedA = a.replace(articlesRegex, '').trim().toLowerCase();
-      const trimmedB = b.replace(articlesRegex, '').trim().toLowerCase();
-      return trimmedA.localeCompare(trimmedB);
+    // Check if the list is sorted and without articles
+    cy.get('#bands li').then(tags => {
+      const tagContent = tags.map((index, tag) => Cypress.$(tag).text()).get();
+
+      // Check if each tag is sorted and without articles
+      const sortedContent = [...tagContent].sort();
+      for (let i = 0; i < sortedContent.length; i++) {
+        const sentence = sortedContent[i];
+        expect(sentence).to.be.equal(tagContent[i]);
+      }
     });
-
-    return sortedNames;
-  }
-
-  // Call the sortBandNames function to sort the band names
-  const sortedBandNames = sortBandNames(bandNames);
-
-  // Get the ul element with id 'bands'
-  const bandsList = document.querySelector('#bands');
-
-  if (bandsList) {
-    // Clear any existing content inside the ul
-    bandsList.innerHTML = '';
-
-    // Iterate through the sorted band names and add them to the ul as list items
-    sortedBandNames.forEach(band => {
-      const listItem = document.createElement('li');
-      listItem.textContent = band;
-      bandsList.appendChild(listItem);
-    });
-  } else {
-    console.error("Element with id 'bands' not found.");
-  }
+  });
 });
