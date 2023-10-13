27e3b0e80b5f8d944d3cfaf1a7d6d01451628c51
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-13 22:27:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
-// Array of band names
-let bandNames = [
-  'The Rolling Stones',
-  'Aerosmith',
-  'The Beatles',
-  'Guns N\' Roses',
-  'Red Hot Chili Peppers',
-  'AC/DC'
-];
+document.addEventListener('DOMContentLoaded', () => {
+  // Array of band names
+  let bandNames = [
+    'The Rolling Stones',
+    'Aerosmith',
+    'The Beatles',
+    'Guns N\' Roses',
+    'Red Hot Chili Peppers',
+    'AC/DC'
+  ];
 
-// Function to sort band names without articles
-function sortBandNames(names) {
-  const articlesRegex = /^(a |an |the )/i;
+  // Function to sort band names without articles
+  function sortBandNames(names) {
+    const articlesRegex = /^(a |an |the )/i;
 
-  // Sort the names without articles
-  const sortedNames = names.sort((a, b) => {
-    const trimmedA = a.replace(articlesRegex, '').toLowerCase();
-    const trimmedB = b.replace(articlesRegex, '').toLowerCase();
-    return trimmedA.localeCompare(trimmedB);
-  });
+    // Sort the names without articles
+    const sortedNames = names.sort((a, b) => {
+      const trimmedA = a.replace(articlesRegex, '').toLowerCase();
+      const trimmedB = b.replace(articlesRegex, '').toLowerCase();
+      return trimmedA.localeCompare(trimmedB);
+    });
 
-  return sortedNames;
-}
+    return sortedNames;
+  }
 
-// Call the sortBandNames function to sort the band names
-const sortedBandNames = sortBandNames(bandNames);
+  // Call the sortBandNames function to sort the band names
+  const sortedBandNames = sortBandNames(bandNames);
 
-// Get the ul element with id 'bands'
-const bandsList = document.querySelector('#bands');
+  // Get the ul element with id 'bands'
+  const bandsList = document.querySelector('#bands');
 
-// Clear any existing content inside the ul
-bandsList.innerHTML = '';
+  if (bandsList) {
+    // Clear any existing content inside the ul
+    bandsList.innerHTML = '';
 
-// Iterate through the sorted band names and add them to the ul as list items
-sortedBandNames.forEach(band => {
-  const listItem = document.createElement('li');
-  listItem.textContent = band;
-  bandsList.appendChild(listItem);
+    // Iterate through the sorted band names and add them to the ul as list items
+    sortedBandNames.forEach(band => {
+      const listItem = document.createElement('li');
+      listItem.textContent = band;
+      bandsList.appendChild(listItem);
+    });
+  } else {
+    console.error("Element with id 'bands' not found.");
+  }
 });
