af60d8d9f0fc92774cb29aa294691a07326616d3
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-17 20:30:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,52 @@
-// Function to remove articles from a string
-function getArticleLessString(input) {
-  let words = input.split(' ');
-  let nonArticleWords = words.filter((word) => {
-    let smallCaseWord = word.toLowerCase();
-    return !(smallCaseWord === 'the' || smallCaseWord === 'an' || smallCaseWord === 'a');
-  });
-  return nonArticleWords.join(' ');
-}
+document.addEventListener('DOMContentLoaded', () => {
+  // Function to remove articles from a string
+  function getArticleLessString(input) {
+    let words = input.split(' ');
+    let nonArticleWords = words.filter((word) => {
+      let smallCaseWord = word.toLowerCase();
+      return !(smallCaseWord === 'the' || smallCaseWord === 'an' || smallCaseWord === 'a');
+    });
+    return nonArticleWords.join(' ');
+  }
 
-// Array of band names
-let arr = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
+  // Array of band names
+  let arr = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
 
-// Create a hash map to store article-less strings as keys and original strings as values
-let hashMap = {};
+  // Create a hash map to store article-less strings as keys and original strings as values
+  let hashMap = {};
 
-for (let i = 0; i < arr.length; i++) {
-  let articleLessString = getArticleLessString(arr[i]);
-  hashMap[articleLessString] = arr[i];
-}
+  for (let i = 0; i < arr.length; i++) {
+    let articleLessString = getArticleLessString(arr[i]);
+    hashMap[articleLessString] = arr[i];
+  }
 
-// Get article-less strings and sort them
-let articleLessStrings = Object.keys(hashMap);
-articleLessStrings.sort();
+  // Get article-less strings and sort them
+  let articleLessStrings = Object.keys(hashMap);
+  articleLessStrings.sort();
 
-// Get the ul element with id 'bands'
-const bandsList = document.querySelector('#bands');
+  // Wait for the bandsList element to be available in the DOM
+  const waitForBandsList = setInterval(() => {
+    const bandsList = document.querySelector('#bands');
+    if (bandsList) {
+      clearInterval(waitForBandsList);
 
-if (bandsList) {
-  // Clear any existing content inside the ul
-  bandsList.innerHTML = '';
+      // Clear any existing content inside the ul
+      bandsList.innerHTML = '';
 
-  // Iterate through the sorted band names and add them to the ul as list items
-  articleLessStrings.forEach(newString => {
-    const band = hashMap[newString];
+      // Iterate through the sorted band names and add them to the ul as list items
+      articleLessStrings.forEach(newString => {
+        const band = hashMap[newString];
 
-    // Create a new li element
-    const listItem = document.createElement('li');
+        // Create a new li element
+        const listItem = document.createElement('li');
 
-    // Set the text content of the li element to the band name
-    listItem.textContent = band;
+        // Set the text content of the li element to the band name
+        listItem.textContent = band;
 
-    // Append the li element to the ul
-    bandsList.appendChild(listItem);
-  });
-} else {
-  console.error("Element with id 'bands' not found.");
-}
+        // Append the li element to the ul
+        bandsList.appendChild(listItem);
+      });
+    }
+  }, 100); // Check every 100 milliseconds
+
+});
